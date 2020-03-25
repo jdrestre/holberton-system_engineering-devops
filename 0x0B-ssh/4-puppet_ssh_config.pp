0d8a22dceabfd601aa853bb38539dc8f6057b57b
jdrestre <jdrestre79@gmail.com>
2020-03-24 23:03:31 -0500

    search other option to add line into file ssh config

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,10 +1,6 @@
 # modify ssh config file
-file_line { 'change password authentication':
-  path => '/etc/ssh/ssh_config',
-  line => 'PasswordAuthentication no',
-}
-
-file_line { 'add line modify path IdFile':
-  path => '/etc/ssh/ssh_config',
-  line => 'IdentifyFile ~/.ssh/holberton',
+exec {'/etc/ssh/ssh_config':
+path     => '/bin',
+command  => 'echo "    IdentityFile ~/.ssh/holberton\n    PasswordAuthentication no" >> /etc/ssh/ssh_config',
+provider => 'shell',
 }
