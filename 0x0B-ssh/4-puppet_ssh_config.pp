879349c74012f9fb362168bf6b9b8e937aba9d55
jdrestre <jdrestre79@gmail.com>
2020-03-24 22:56:35 -0500

    correction space identation

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,11 +1,10 @@
 # modify ssh config file
-
-file_line { 'add line modify password authentication':
-    path => '/etc/ssh/ssh_config',
-    line => 'PasswordAuthentication no',
+file_line { 'change password authentication':
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no',
 }
 
 file_line { 'add line modify path IdFile':
-    path => '/etc/ssh/ssh_config',
-    line => 'IdentifyFile ~/.ssh/holberton',
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentifyFile ~/.ssh/holberton',
 }
